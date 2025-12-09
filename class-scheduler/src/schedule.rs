f56e1ddf1b461f8f074c559997a318536a25a71a
sidabdul <sidali@pdx.edu>
2025-12-08 17:45:37 -0800

    Added time struct

---
+++ class-scheduler/src/schedule.rs
@@ -0,0 +1,9 @@
+use crate::day::Day;
+use crate::model::{Course, Meeting};
+use crate::time::Time;
+
+#[derive(Default)]
+pub struct Schedule {
+    pub courses: Vec<Course>,
+}
+
---
+++ class-scheduler/src/time.rs
@@ -0,0 +1,3 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Time(pub u16);
+
